de34633c693e66436376369a37c1b46f3b7a1da7
unknown <adntouch@gmail.com>
2022-06-02 11:55:01 +0200

    add bdd service

--- functions/src/bdd/index.ts
+++ functions/src/bdd/index.ts
@@ -1,18 +1,4 @@
-import type {EntityManager, MySqlDriver} from "@mikro-orm/mysql";
-import {MikroORM} from "@mikro-orm/core";
-import {User} from "./entities";
-import {UserHandler} from "./entityHandlers"; // or any other SQL driver package
+import {UserHandler} from "./entityHandlers";
+import {BddService} from "../services/BddService";
 
-const orm = await MikroORM.init<MySqlDriver>({
-  entities: [User], // path to your JS entities (dist), relative to `baseDir`
-  dbName: "test",
-  host: "nn26812-001.dbaas.ovh.net",
-  user: "secrethouse",
-  password: "03c4s4wC3jqLaCgYIARAAGAMSNwF",
-  port: 35166,
-  type: "mysql",
-});
-
-console.log(orm.em); // access EntityManager via `em` property
-export const entityManager = orm.em as EntityManager;
-export const userHandler = new UserHandler(entityManager);
+export const userHandler = new UserHandler(BddService.entityManager);
---
+++ functions/src/services/BddService.ts
@@ -0,0 +1,29 @@
+import {User} from "../bdd/entities";
+import {EntityManager, MySqlDriver, SqlEntityManager} from "@mikro-orm/mysql";
+import {MikroORM} from "@mikro-orm/core";
+
+export class BddService {
+  private static em: EntityManager|null = null;
+  private static orm: MikroORM<MySqlDriver>;
+  static get entityManager():EntityManager {
+    if (this.em == null) {
+      this.createOrm();
+    }
+    return <SqlEntityManager> this.em;
+  }
+
+  private static createOrm() {
+    MikroORM.init<MySqlDriver>({
+      entities: [User],
+      dbName: "test",
+      host: "nn26812-001.dbaas.ovh.net",
+      user: "secrethouse",
+      password: "03c4s4wC3jqLaCgYIARAAGAMSNwF",
+      port: 35166,
+      type: "mysql",
+    }).then((r)=> {
+      this.orm = r;
+      this.em = this.orm.em;
+    });
+  }
+}
