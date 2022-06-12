c6eb96e3c7188b3f4159184f86fbb60e18c1c8b8
Romain Fréchet <romainfpro@gmail.com>
2022-06-12 10:30:51 +0200

    feat :sparkles: Add Room entity and Handler

---
+++ functions/src/bdd/entityHandlers/RoomHandler.ts
@@ -0,0 +1,9 @@
+import {EntityHandler} from "./EntityHandler";
+import {Room} from "../entities";
+import {EntityManager} from "@mikro-orm/mysql";
+
+export class RoomHandler extends EntityHandler {
+    constructor(entityManager: EntityManager) {
+        super(entityManager, Room);
+    }
+}
