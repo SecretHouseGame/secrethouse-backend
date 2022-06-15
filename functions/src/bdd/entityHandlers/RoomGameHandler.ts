e74a6fe40ec7e641091ea819a62fc09cdd3ecd4a
Romain Fréchet <romainfpro@gmail.com>
2022-06-15 14:44:03 +0200

    fix: remove RoomGameHandler create

--- functions/src/bdd/entityHandlers/RoomGameHandler.ts
+++ functions/src/bdd/entityHandlers/RoomGameHandler.ts
@@ -1,15 +1,9 @@
 import {Game, RoomGame, Room} from "../entities";
 import {EntityManager} from "@mikro-orm/mysql";
 import {EntityHandler} from "./EntityHandler";
-import {castToRoomGameData} from "../../types/request/bodyData";
 
 export class RoomGameHandler extends EntityHandler { 
     constructor(entityManager: EntityManager) {
         super(entityManager, RoomGame)
-    }
-    
-    async createRoomGame(payload: any, game: Game, room: Room) {
-        const RoomGameData = castToRoomGameData(payload);
-        
     }
 }
