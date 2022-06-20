af65ed689e7b96f8a8a0afc9220c5a3dd798c599
Romain Fréchet <58610934+Hikari-rom@users.noreply.github.com>
2022-06-20 15:02:10 +0200

    Fix Mikrorm Config ts

--- functions/src/mikroOrm.config.ts
+++ functions/src/mikroOrm.config.ts
@@ -3,14 +3,19 @@ import {TsMorphMetadataProvider} from "@mikro-orm/reflection";
 import {Options} from "@mikro-orm/core";
 
 const options = function(): Options {
+  const functions = require('firebase-functions');
+  const config = functions.config();
+  for(const key in config.envs){
+    process.env[key.toUpperCase()] = config.envs[key];
+  }
   return {
     entities: [User, Game, Room, RoomGame, Player, Event, Buzz, Nomination, Vote],
-    dbName: process.env.db_name,
-    host: process.env.db_host,
-    user: process.env.db_user,
-    password: process.env.db_password,
+    dbName: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     metadataProvider: TsMorphMetadataProvider,
-    port: Number(process.env.db_port),
+    port: Number(process.env.DB_PORT),
     type: "mysql",
   };
 };
