524bb7df9bd10a60f4989c8bfaffd9256b30bd47
Nicolas Notararigo <nicolas.notararigo@ynov.com>
2022-06-21 11:34:40 +0200

    fix: variable environment

--- functions/src/mikroOrm.config.ts
+++ functions/src/mikroOrm.config.ts
@@ -1,17 +1,16 @@
 import {Buzz, Event, Game, Nomination, Player, Room, RoomGame, User, Vote} from "./bdd/entities";
 import {TsMorphMetadataProvider} from "@mikro-orm/reflection";
 import {Options} from "@mikro-orm/core";
-import * as functions from "firebase-functions";
 
 const options = function(): Options {
   return {
     entities: [User, Game, Room, RoomGame, Player, Event, Buzz, Nomination, Vote],
-    dbName: functions.config().db_name,
-    host: functions.config().db_host,
-    user: functions.config().db_user,
-    password: functions.config().db_password,
+    dbName: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     metadataProvider: TsMorphMetadataProvider,
-    port: Number(functions.config().db_port),
+    port: Number(process.env.DB_PORT),
     type: "mysql",
   };
 };
