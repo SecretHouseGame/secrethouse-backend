bddcd4aecee26885445d449b711a94027a119306
Nicolas Notararigo <nicolas.notararigo@ynov.com>
2022-06-21 12:00:29 +0200

    fix: test env for firebase

--- functions/src/mikroOrm.config.ts
+++ functions/src/mikroOrm.config.ts
@@ -1,16 +1,17 @@
 import {Buzz, Event, Game, Nomination, Player, Room, RoomGame, User, Vote} from "./bdd/entities";
 import {TsMorphMetadataProvider} from "@mikro-orm/reflection";
 import {Options} from "@mikro-orm/core";
+import * as functions from "firebase-functions";
 
 const options = function(): Options {
   return {
     entities: [User, Game, Room, RoomGame, Player, Event, Buzz, Nomination, Vote],
-    dbName: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    dbName: functions.config().db_name,
+    host: functions.config().db_host,
+    user: functions.config().db_user,
+    password: functions.config().db_password,
     metadataProvider: TsMorphMetadataProvider,
-    port: Number(process.env.DB_PORT),
+    port: Number(functions.config().db_port),
     type: "mysql",
   };
 };
